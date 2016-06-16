691516fb50473d86d7dbc268ada1adf20278c81b
snatow <natow.sarah@gmail.com>
2016-06-16 12:33:50 -0400

    adding routes

--- controllers/root.js
+++ controllers/root.js
@@ -1,7 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', function(req, res){
+  console.log("root");
+  res.send("root");
+});
 
-
+router.get("/joe", function(req, res) {
+  console.log("joe");
+  res.send("joe");
+})
 
 module.exports = router;
