86bb327d353aa46c9caf95a76c0d6a9534c0e490
snatow <natow.sarah@gmail.com>
2016-06-16 12:30:28 -0400

    making fix

--- controllers/root.js
+++ controllers/root.js
@@ -2,16 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 
-var clientID = 'Client-ID ' + process.env.IMGUR_CLIENT_ID
 
-router.get('/', function(req, res){
-  console.log("root");
-  res.send("root");
-});
-
-router.get("/joe", function(req, res) {
-  console.log("joe");
-  res.send("joe");
-})
 
 module.exports = router;
