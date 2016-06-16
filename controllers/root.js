b35e1f7e07ffb7aab6377e533375b9afe47478ee
Shaked Edri <ShakedEdri@Shakeds-MacBook-Pro.local>
2016-06-16 12:36:32 -0400

    adding david route

--- controllers/root.js
+++ controllers/root.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 
+router.get("/david", function(req, res) {
+  console.log("david");
+  res.send("david");
+})
+
 
 
 module.exports = router;
